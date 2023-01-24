1f11483a8f31f850a20e9b5bff601e48f96d7d5c
ColtMort <balding.fenders.0e@icloud.com>
2023-01-23 21:05:44 -0700

    edamam api display image and title of recipe

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,25 +1,29 @@
 let edamamApiURL = 'https://api.edamam.com/api/recipes/v2?type=public&app_id=61bbe3eb&app_key=8ee095c4c39545389e1a1212758a8ac8&imageSize=REGULAR&random=true&field=label&field=images&field=source&field=url&field=mealType'
 let recievedRecipes = {}
 let nextRecipeClickCounter = 0
+let recipeTitle = document.querySelector('.recipe-name')
+let recipeImage = document.querySelector('.recipe-image')
+
+
 
 function getEdamamApi(){
-    return new Promise(()=>
+    return new Promise(function(resolve, reject){
     fetch(edamamApiURL)
-    .then((response)=> {
-        console.log(response.status)
-       response.json().then(function (data) {
-            recievedRecipes = data.hits
-            console.log(recievedRecipes)
-       })}))
+    .then(response => 
+       response.json()
+    .then(data => 
+        resolve(recievedRecipes = data.hits)
+       ))})
 }
-
-
-getEdamamApi().then(displayRecipe())
 
 function displayRecipe() {
     nextRecipeClickCounter++
-    console.log('display ' + recievedRecipes)
-    let recipeImageURL = recievedRecipes[0].recipe.images.REGULAR.url
-    console.log(recipeImageURL)
-
+    recipe = recievedRecipes[nextRecipeClickCounter].recipe
+    console.log(recipe)
+    recipeImage.setAttribute('src', recipe.images.REGULAR.url)
+    recipeTitle.innerText = recipe.label
+    
 }
+
+getEdamamApi()
+    .then(displayRecipe)
